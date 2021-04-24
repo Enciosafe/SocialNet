d8a1cd38805fcac7e1b82d9107d0cd8fa35ac725
Stanislav Matvieiev <enciosafe@gmail.com>
2021-04-24 19:31:48 +0300

    lesson#20

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -5,11 +5,11 @@ import {NavLink} from "react-router-dom";
 
 const Nav = () => {
     return <nav className={styles.nav}>
-        <div className={styles.item}><NavLink to="/profile">Profile</NavLink></div>
-        <div className={styles.item}><NavLink to="/dialogs">Messages</NavLink></div>
-        <div className={styles.item}><NavLink to="/news">News</NavLink></div>
-        <div className={styles.item}><NavLink to="/music">Music</NavLink></div>
-        <div className={styles.item}><NavLink to="/settings">Settings</NavLink></div>
+        <div className={styles.item}><NavLink to="/profile" activeClassName={styles.active} >Profile</NavLink></div>
+        <div className={styles.item}><NavLink to="/dialogs" activeClassName={styles.active} >Messages</NavLink></div>
+        <div className={styles.item}><NavLink to="/news" activeClassName={styles.active} >News</NavLink></div>
+        <div className={styles.item}><NavLink to="/music" activeClassName={styles.active} >Music</NavLink></div>
+        <div className={styles.item}><NavLink to="/settings" activeClassName={styles.active} >Settings</NavLink></div>
     </nav>;
 }
 
