74964b89400c7d58060c374b4870c79ec4d3e01e
Stanislav Matvieiev <enciosafe@gmail.com>
2021-05-01 13:43:16 +0300

    lesson #38

--- src/App.js
+++ src/App.js
@@ -24,8 +24,7 @@ const App = (props) => {
               <div className='app-wrapper-content'>
                   <Route path='/dialogs' render={ () => <Dialogs state={props.state.dialogsPage}/>} />
                   <Route path='/profile' render={ () => <Profile profilePage={props.state.profilePage}
-                                                                 updateNewPostText={props.updateNewPostText}
-                                                                 addPost={props.addPost}/>} />
+                                                                 dispatch={props.dispatch}/>} />
                   <Route path='/friends' render={ () => <Friends state={props.state.friendsBar} />} />
                   <Route path='/news' component={News} />
                   <Route path='/music' component={Music} />
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -11,8 +11,7 @@ const Profile = (props) => {
             <ProfileInfo />
             <MyPost postData={props.profilePage.postData}
                     newPostText={props.profilePage.newPostText}
-                    updateNewPostText={props.updateNewPostText}
-                    addPost={props.addPost}/>
+                    dispatch={props.dispatch}/>
         </div>
     )
 
