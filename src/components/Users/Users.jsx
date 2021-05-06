653029ba6b68b9a38fe92abccb345e38430f80d5
Stanislav Matvieiev <enciosafe@gmail.com>
2021-05-06 16:28:25 +0300

    lesson #51

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,17 +1,14 @@
 import React from "react";
 import styles from "./Users.module.css"
+import * as axios from "axios";
+import userPhoto from "../../assets/images/user.png"
 
 const Users = (props) => {
 
     if (props.users.length === 0) {
-        props.setUsers([
-            {id: '1', photoUrl: 'https://skolkolet.com/images/590e5ad27d66ac348e88c00d/dmitriy-nagiev.jpg', followed: false, fullname: 'Stas Matvieiev', status: 'i am a boss', location: {city: 'Toronto', country: 'Canada'} },
-            {id: '2', photoUrl: 'https://skolkolet.com/images/590e5ad27d66ac348e88c00d/dmitriy-nagiev.jpg', followed: false,fullname: 'Igor Lastochkin', status: 'adafhehehe googls suks', location: {city: 'Kiev', country: 'Ukraine'} },
-            {id: '3', photoUrl: 'https://skolkolet.com/images/590e5ad27d66ac348e88c00d/dmitriy-nagiev.jpg', followed: true,fullname:'Olga Orlova', status: 'ilisteN!!!', location: {city: 'Moskow', country: 'Russia'} },
-            {id: '4', photoUrl: 'https://skolkolet.com/images/590e5ad27d66ac348e88c00d/dmitriy-nagiev.jpg', followed: true,fullname:'Garick Harlamov', status: 'i want coffie', location: {city: 'Paris', country: 'Franch'} },
-            {id: '5', photoUrl: 'https://skolkolet.com/images/590e5ad27d66ac348e88c00d/dmitriy-nagiev.jpg', followed: false,fullname: 'Irina Podpolnaya', status: 'fuck off', location: {city: 'Toronto', country: 'Canada'} },
-            {id: '6', photoUrl: 'https://skolkolet.com/images/590e5ad27d66ac348e88c00d/dmitriy-nagiev.jpg', followed: true,fullname: 'Jmix ', status: 'asdadq!', location: {city: 'Dubai', countrwy: 'Emirates'} },
-        ])
+        axios.get(' https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            props.setUsers(response.data.items)
+        })
     }
 
 
@@ -20,7 +17,7 @@ const Users = (props) => {
             props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photoUrl} className={styles.userPhoto}/>
+                        <img src={u.photos.small !== null ? u.photos.small : userPhoto } className={styles.userPhoto}/>
                     </div>
                     <div>
                         {u.followed
@@ -30,12 +27,12 @@ const Users = (props) => {
                 </span>
                 <span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{"u.location.country"}</div>
+                        <div>{"u.location.city"}</div>
                     </span>
                 </span>
             </div>)
