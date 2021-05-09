dd423933a72758c113b834371b295923aee93c80
Stanislav Matvieiev <enciosafe@gmail.com>
2021-05-10 00:54:15 +0300

    lesson #53

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -11,6 +11,7 @@ const Nav = (props) => {
         <div className={styles.item}><NavLink to="/news" activeClassName={styles.active} >News</NavLink></div>
         <div className={styles.item}><NavLink to="/music" activeClassName={styles.active} >Music</NavLink></div>
         <div className={styles.item}><NavLink to="/settings" activeClassName={styles.active} >Settings</NavLink></div>
+        <div className={styles.item}><NavLink to="/users" activeClassName={styles.active} >Users</NavLink></div>
 
     </nav>;
 }
