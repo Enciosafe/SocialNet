3998020f86ce1af84abcc6757ce6f47ed6f9028f
Stanislav Matvieiev <enciosafe@gmail.com>
2021-05-14 16:29:11 +0300

    lesson #57

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -5,13 +5,12 @@ import {
     setCurrentPageAC,
     setTotalUsersCountAC,
     setUsersAC,
-    toggleIsFetching, toggleIsFetchingAC,
+    toggleIsFetchingAC,
     unfollowAC
 } from "../../Redux/users-reducer";
 import * as axios from "axios";
 import Users from "./Users";
-import preloader from "../../assets/images/Spinner-1.6s-200px.svg";
-
+import Preloader from "../common/Preloader/Preloader";
 
 
 class UsersComponent extends React.Component {
@@ -37,7 +36,7 @@ class UsersComponent extends React.Component {
 
     render() {
         return <>
-            { this.props.isFetching ? <img src={preloader} /> : null }
+            { this.props.isFetching ? <Preloader /> : null }
             <Users totalUsersCount={this.props.totalUsersCount}
                    pageSize={this.props.pageSize}
                    currentPage={this.props.currentPage}
---
+++ src/components/common/Preloader/Preloader.js
@@ -0,0 +1,8 @@
+import preloader from "../../../assets/images/Spinner-1.6s-200px.svg";
+import React from "react";
+
+let Preloader = (props) => {
+    return <img src={preloader} />
+}
+
+export default Preloader
